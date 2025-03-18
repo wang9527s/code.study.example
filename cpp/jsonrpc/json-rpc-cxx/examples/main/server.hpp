25a453bd4cf5182dea13ad5af27d5ac6b3c1656e
wangbin <2722620236@qq.com>
2025-03-18 21:07:23 +0800

    添加calc函数

--- cpp/jsonrpc/json-rpc-cxx/examples/main/server.hpp
+++ cpp/jsonrpc/json-rpc-cxx/examples/main/server.hpp
@@ -36,6 +36,10 @@ public:
             result.push_back(p.second);
         return result;
     }
+    int calc(int a, int b)
+    {
+        return a + b;
+    }
 
 private:
     std::map<std::string, Product> products;
@@ -49,6 +53,7 @@ public:
         rpcServer.Add("GetProduct", GetHandle(&ServerHandle::GetProduct, app), {"id"});
         rpcServer.Add("AddProduct", GetHandle(&ServerHandle::AddProduct, app), {"product"});
         rpcServer.Add("AllProducts", GetHandle(&ServerHandle::AllProducts, app), {});
+        rpcServer.Add("calc", GetHandle(&ServerHandle::calc, app), {"int", "int"});
 
         httpServer = new CppHttpLibServerConnector(rpcServer, 8484);
         std::cout << "Starting http server: " << std::boolalpha << httpServer->StartListening() << "\n";
