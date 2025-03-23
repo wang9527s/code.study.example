f15bb070ccc0bdde8dac5f3f2ce190420dc1e140
wangbin <2722620236@qq.com>
2025-03-23 22:33:31 +0800

    完善定位问题方法:
     1. 使用loguru日志系统
     2. linux上默认debug模式，gdb 调试（workspaces是个好东西）

--- cpp/jsonrpc/json-rpc-cxx/build.sh
+++ cpp/jsonrpc/json-rpc-cxx/build.sh
@@ -9,9 +9,12 @@ mkdir tmp-build
 
 cd tmp-build
 
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+# 默认 release
 cmake ..
 
 echo "make -j$(nproc)"
 make -j$(nproc)
 
+# gdb ./ex-rpc
 ./ex-rpc
--- cpp/jsonrpc/json-rpc-cxx/examples/main/main.cpp
+++ cpp/jsonrpc/json-rpc-cxx/examples/main/main.cpp
@@ -1,4 +1,4 @@
-#include "client.hpp"
+﻿#include "client.hpp"
 #include "conn/cpphttplibconnector.hpp"
 #include "conn/inmemoryconnector.hpp"
 #include "server.hpp"
@@ -8,14 +8,27 @@
 #include <jsonrpccxx/server.hpp>
 #include <vector>
 
+#include <log/loguru.hpp>
+
 using namespace jsonrpccxx;
 using namespace std;
 
-
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Running http example"
-         << "\n";
+    loguru::g_preamble_date      = false;  // 是否显示日期
+    loguru::g_preamble_time      = true;  // 是否显示时间
+    loguru::g_preamble_uptime    = true;  // 是否显示运行时
+    loguru::g_preamble_thread    = true;  // 是否显示线程
+    loguru::g_preamble_file      = false;  // 是否显示文件
+    loguru::g_preamble_verbose   = false;  // 是否显示级别
+    loguru::g_preamble_pipe      = false;  // 是否加上管道符|
+    loguru::init(argc, argv);
+
+    // 设置日志文件
+    loguru::add_file("logfile.txt", loguru::Append, loguru::Verbosity_INFO);
+    LOG_F(INFO, "Running http example");
+
+
     AppServer ser;
 
     std::this_thread::sleep_for(1s);
